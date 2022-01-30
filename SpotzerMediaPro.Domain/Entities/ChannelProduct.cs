abe656bc6e3d970cfa5b2b1a5ff529bdaf34217a
Kayode Ademolaji <kayode@rb2.ng>
2022-01-30 01:36:23 +0100

    Added Domain entities

---
+++ SpotzerMediaPro.Domain/Entities/ChannelProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotzerMediaPro.Domain.Entities
+{
+    public class ChannelProduct: BaseEntity<long>
+    {
+        public int ProductId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ SpotzerMediaPro.Domain/Entities/OrderLineItemAdwordCampaign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotzerMediaPro.Domain.Entities
+{
+    public class OrderLineItemAdwordCampaign: BaseEntity<Guid>
+    {
+        public Guid OrderLineItemId { get; set; }
+        public string CampaignName { get; set; }
+        public string CampaignAddressLine1 { get; set; }
+        public string CampaignPostCode { get; set; }
+        public string CampaignRadius { get; set; }
+        public string LeadPhoneNumber { get; set; }
+        public string SMSPhoneNumber { get; set; }
+        public string UniqueSellingPoint1 { get; set; }
+        public string UniqueSellingPoint2 { get; set; }
+        public string UniqueSellingPoint3 { get; set; }
+        public string Offer { get; set; }
+        public string DestinationURL { get; set; }
+    }
+}
