1ab650f8ed43fe537cda40fd716ed8c0fe0e6365
Kayode Ademolaji <kayode@rb2.ng>
2022-02-11 11:11:17 +0100

    Ignore Json null value

--- SpotzerMediaPro.Contracts/DataContracts/Order/OrderLineItemDto.cs
+++ SpotzerMediaPro.Contracts/DataContracts/Order/OrderLineItemDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,9 @@ namespace SpotzerMediaPro.Contracts.DataContracts.Order
 {
     public class OrderLineItemDto : BaseOrderLineItems
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public OrderLineItemWebSiteDetailDto WebsiteDetails { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public OrderLineItemAdwordCampaignDto AdwordCampaign { get; set; }
     }
 }
